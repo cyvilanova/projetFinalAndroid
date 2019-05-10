3a05bc92a06a70dfe6619380cc41814d96e04067
DavidGau <gaulindavidweb@gmail.com>
2019-05-10 15:49:08 -0400

    Added a side bar, still working on it

--- app/src/main/java/com/example/quintessentiel/MainActivity.java
+++ app/src/main/java/com/example/quintessentiel/MainActivity.java
@@ -1,22 +1,61 @@
 package com.example.quintessentiel;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutCompat;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    private DrawerLayout drawer;
+    private ActionBarDrawerToggle toggle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.sign_up);
+        setContentView(R.layout.connection);
 
-        /* Lines to use when we no longer need an icon from the menu
-        ImageView t = findViewById(R.id.toolBarLeftBlockImage);
-        t.setVisibility(View.INVISIBLE);
-        */
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        findViewById(R.id.drawer_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // open right drawer
+                DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+                drawer.openDrawer(GravityCompat.END);
+                System.out.println("OPENING");
+
+            }
+        });
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.bringToFront();
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                System.out.println(menuItem);
+                return false;
+            }
+        });
+        // NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        // navigationView.setNavigationItemSelectedListener(this);
+
+    /*
+        toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigationDrawerOpen,R.string.navigationDrawerClose);
+        drawer.addDrawerListener(toggle);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+     */
+
+
+
     }
 }
